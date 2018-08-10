3cf683b710c33f8c2ea0bd2f28e85b0c50aa56eb
smartbrave <779850695@qq.com>
2018-08-10 05:48:36 -0400

    optimization

--- CanJump/func.go
+++ CanJump/func.go
@@ -5,18 +5,13 @@ func canJump(nums []int) bool {
 	if length == 0 {
 		return true
 	}
-	dp := make([]bool, length)
-	dp[0] = true
 	arrive := nums[0]
 	for i := 1; i < length; i++ {
 		if arrive >= i {
-			dp[i] = true
-		}
-		if dp[i] {
 			if i+nums[i] > arrive {
 				arrive = i + nums[i]
 			}
 		}
 	}
-	return dp[length-1]
+	return arrive >= length-1
 }
