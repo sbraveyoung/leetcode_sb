52f9b5f887070433a875c004a39146d905f154d3
SmartBrave <SmartBraveCoder@gmail.com>
2020-11-25 20:31:52 +0800

    222CountNodes

---
+++ 222CountNodes/func_test.go
@@ -0,0 +1,90 @@
+package countNodes
+
+import "testing"
+
+func Test_countNodes(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "first",
+			args: args{
+				root: &TreeNode{
+					Val: 1,
+					Left: &TreeNode{
+						Val: 2,
+						Left: &TreeNode{
+							Val: 4,
+						},
+						Right: &TreeNode{
+							Val: 5,
+						},
+					},
+					Right: &TreeNode{
+						Val: 3,
+						Left: &TreeNode{
+							Val: 6,
+						},
+					},
+				},
+			},
+			want: 6,
+		},
+		{
+			name: "second",
+			args: args{
+				root: &TreeNode{
+					Val: 1,
+					Left: &TreeNode{
+						Val: 2,
+						Left: &TreeNode{
+							Val: 4,
+						},
+						Right: &TreeNode{
+							Val: 5,
+						},
+					},
+					Right: &TreeNode{
+						Val: 3,
+						Left: &TreeNode{
+							Val: 6,
+						},
+						Right: &TreeNode{
+							Val: 7,
+						},
+					},
+				},
+			},
+			want: 7,
+		},
+		{
+			name: "third",
+			args: args{
+				root: &TreeNode{
+					Val: 1,
+				},
+			},
+			want: 1,
+		},
+		{
+			name: "fourth",
+			args: args{
+				root: nil,
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countNodes(tt.args.root); got != tt.want {
+				t.Errorf("countNodes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
